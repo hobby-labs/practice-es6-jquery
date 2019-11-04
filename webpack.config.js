b71f6e09e636a21c3bb62b7718c46eefcb09f8ca
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-11-04 11:58:50 +0900

    Added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.join(__dirname, "src/"),
+  entry: {
+    'index': './js/index.js'
+  },
+  module: {
+    rules: [
+      {
+        "test": /\.js$/,
+        "exclude": /node_modules/,
+        "use": {
+          "loader": "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    // Provides jQuery for other JS bundled with Webpack
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname + "/public/"    ,
+    publicPath: "/",
+    filename: "[name].js"
+  },
+  // Re loading trick when not only *.js files are modified but also *.html files.
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    watchContentBase: true,
+    hot: true,
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  }
+};
+
+
