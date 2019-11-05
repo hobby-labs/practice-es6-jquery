c1546af484ec7adbafbbcc937a6841bd64152a09
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-11-05 21:19:45 +0900

    Updated

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,20 @@ import "./test2"
 //import { jAlert } "jAlert";
 import "jAlert";
 
-window.onload = () => {
+$(document).ready(() => {
   console.log("Hello world.");
   //let test1 = new Test1();
   console.log($.test2);
   console.log($.test2());
-  console.log("===================================");
-  console.log($.jAlert);
-  window.jAlert = $.jAlert;
-  console.log("===================================");
-}
+  $(function(){
+    $.jAlert({
+      'title': 'Nice Size',
+      'content': 'This is the  sized alert',
+      'size': 'sm',
+       'theme':'blue',
+      'closeOnClick': true 
+    });
+
+  });
+});
 
